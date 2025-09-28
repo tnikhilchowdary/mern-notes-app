4d1b997f5e93d58466f143c79e8ca7958892a644
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-09-28 09:17:00 -0500

    Frontend Setup

---
+++ frontend/notes-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Notes App</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
