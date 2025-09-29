03945d1c1fb47cfb6dce5ab628500dbe3b4cba3f
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-09-29 12:10:46 -0500

    Creating Note

--- frontend/notes-app/src/App.js
+++ frontend/notes-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NoteForm from "./pages/NoteForm";
+import CreateNote from "./pages/CreateNote";
+
 
 function App() {
   return (
     <div className="App">
       <h1>Notes App</h1>
-      <NoteForm />
+      <CreateNote />
     </div>
   );
 }
---
+++ frontend/notes-app/src/pages/CreateNote.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from "react";
+
+const CreateNote = () => {
+    return(
+        <div>
+            <h1>Creating Note</h1>
+        </div>
+    )
+}
+
+
+export default CreateNote;
